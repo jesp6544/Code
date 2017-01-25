561325ab5b07abb2bdcc19889c8eac48b2d9d652
jesp6544 <jesp6544@live.dk>
2017-01-25 13:56:35 +0100

    Css name changed. Comments added were needed. Site made reletivly dynamic.

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
 <html>
 <header>
-    <link rel="stylesheet" type="text/css" href="bla.css">
-    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="MyStyle.css">
+    <meta charset="UTF-8"> </meta>
 </header>
 <body>
 <div class="outer-div">
 
 <?php 
-//echo '<p>Hello World</p>';
-$files = glob('*',GLOB_ONLYDIR);
+$files = glob('*',GLOB_ONLYDIR);    //glob grabs all elements in the folder, _ONLYDIR, it only takes folders
 foreach ($files as $i){
-    $fileInDir = glob($i . '/*.png');
+    $fileInDir = glob($i . '/*.png');   //grabs all .png's
     foreach ($fileInDir as $temp){
-        //echo $i , ' has: ', $temp; //,"<br />"
-        //echo $tem //bla
         ?>
         <div class="inner-div">
-            <img src="<?php echo $temp;?>" width="400" height="200" title="Logo of a company" alt="Logo of a company" /> 
-            <h1>An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud. You have so many different things placeholder text has to be able to do, and I don't believe Lorem Ipsum has the stamina.</h1>
+            <img src="<?php echo $temp;?>" class="inner-div" title="Project preview" /> 
+            <h1>Title here</h1>
+            <p>An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud. You have so many different things placeholder text has to be able to do, and I don't believe Lorem Ipsum has the stamina.</p>  
+            <!-- Thx, Trump ipsum: http://trumpipsum.net -->
         </div>
         <?php
     }
