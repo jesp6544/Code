4ab5b54f76d3e96b0485038f829214ffae10f5e6
jesp6544 <jesp6544@live.dk>
2017-01-30 10:18:54 +0100

    Examples cleared up again. Index changed to reflect this change.

--- index.php
+++ index.php
@@ -8,19 +8,29 @@
 <div class="outer-div">
 
 <?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $files = glob('*',GLOB_ONLYDIR);    //glob grabs all elements in the folder, _ONLYDIR, it only takes folders
 foreach ($files as $i){
-    $fileInDir = glob($i . '/*.png');   //grabs all .png's
-    foreach ($fileInDir as $temp){
+    $fileInDir = glob($i . '/*.*');     //grabs all files
+        $txtFile = glob($i . '/*.txt'); //grabs .txt files
+        $picFile = glob($i . '/*.png'); //same
+        $htmlfile = glob($i . '/*.html');   //same
         ?>
         <div class="inner-div">
-            <img src="<?php echo $temp;?>" class="inner-div" title="Project preview" /> 
-            <h1><?php echo basename($temp, '.png')?></h1>
-            <p>An ‘extremely credible source’ has called my office and told me that Barack Obama’s placeholder text is a fraud. You have so many different things placeholder text has to be able to do, and I don't believe Lorem Ipsum has the stamina.</p>  
-            <!-- Thx, Trump ipsum: http://trumpipsum.net -->
+            <a href="<?php echo $htmlfile[0];?>">   <!-- makes pic clickable with link to -->
+            <img src="<?php echo $picFile[0];?>" class="inner-div" title="Project preview" /> 
+            </a>
+            <h1><?php echo basename($txtFile[0], ".txt")?></h1>
+            <p> 
+            <?php 
+            $bla =  file_get_contents ( $txtFile[0] );
+            echo $bla;
+               ?></p>
         </div>
         <?php
-    }
+        
 }
 ?>
 </div>
