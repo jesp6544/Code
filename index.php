dcec592c737a02b923db515d96bfb467c0d29bb6
jesp6544 <jesp6544@live.dk>
2017-01-30 12:58:56 +0100

    Index can now take both links and html files. first real entity added.

--- index.php
+++ index.php
@@ -12,25 +12,45 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 $files = glob('*',GLOB_ONLYDIR);    //glob grabs all elements in the folder, _ONLYDIR, it only takes folders
-foreach ($files as $i){
-    $fileInDir = glob($i . '/*.*');     //grabs all files
-        $txtFile = glob($i . '/*.txt'); //grabs .txt files
-        $picFile = glob($i . '/*.png'); //same
-        $htmlfile = glob($i . '/*.html');   //same
-        ?>
-        <div class="inner-div">
-            <a href="<?php echo $htmlfile[0];?>">   <!-- makes pic clickable with link to -->
-            <img src="<?php echo $picFile[0];?>" class="inner-div" title="Project preview" /> 
+foreach ($files as $i)
+{
+    $fileInDir = glob($i . '/*.*'); //grabs all files
+    $txtFile = glob($i . '/*.txt'); //grabs some files
+    $picFile = glob($i . '/*.png'); //same
+    $htmlfile = glob($i . '/*.html');   //same
+    ?>
+    <div class="inner-div">
+        <?php if (file_get_contents ($htmlfile[0],null,null,null,4) == "http")  //checks if the .html file is a link, yes i know a link in a .html file is wrong, but it works
+        {
+            ?>
+            <a href="<?php echo file_get_contents ($htmlfile[0]);?>">   <!-- makes pic clickable with link to the site -->
+                <img src="<?php echo $picFile[0];?>" class="inner-div" title="Project preview" />   <!-- sets the picture file -->
             </a>
-            <h1><?php echo basename($txtFile[0], ".txt")?></h1>
-            <p> 
+            <?php
+        }
+        else{
+            ?>
+            <a href="<?php echo $htmlfile[0];?>">   <!-- makes pic clickable with link to the html file -->
+                <img src="<?php echo $picFile[0];?>" class="inner-div" title="Project preview" />   <!-- sets the picture file -->
+            </a>
+            <?php
+        }
+        ?>
+        <h1>
+          <?php 
+                echo basename($txtFile[0], ".txt"); //sets the h1 to .txt filename
+           ?>
+        </h1>
+        <p> 
             <?php 
-            $bla =  file_get_contents ( $txtFile[0] );
-            echo $bla;
-               ?></p>
-        </div>
-        <?php
-        
+                $bla =  file_get_contents ( $txtFile[0] );  //gets the contents to a string
+                echo $bla;  //prints that string to the p tag
+            ?>
+        </p>
+    </div>
+    <?php
 }
 ?>
 </div>
+
+<!-- why is php so ugly? -->
